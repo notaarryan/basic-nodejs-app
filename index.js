b39c34b081d62514076d50b668dcd24448f28903
Aryan Parmar <156536547+notaarryan@users.noreply.github.com>
2025-12-26 16:59:32 -0500

    fix: fixed error with resume download

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const https = require("node:https");
+require("dotenv").config({ quiet: true });
 
 app
   .get("/", (req, res) => {
@@ -13,7 +15,19 @@ app
     res.sendFile(path.join(__dirname, "contact-me.html"));
   })
   .get("/resume", (req, res) => {
-    res.download(path.join(__dirname, "private", "AryanParmar.pdf"));
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=AryanParmarResume.pdf"
+    );
+    res.setHeader("Content-Type", "application/pdf");
+
+    https
+      .get(process.env.RESUME_URL, (dropboxRes) => {
+        dropboxRes.pipe(res);
+      })
+      .on("error", () => {
+        res.status(500).send("Resume not available");
+      });
   })
   .use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, "404.html"));
